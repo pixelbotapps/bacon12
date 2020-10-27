54ae3859c4a6a6c7e8752e28c7ce99e071ad337a
PixelBot <56308129+pixelbotapps@users.noreply.github.com>
2020-10-27 15:30:56 +0530

    first

---
+++ include.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'google-api-php-client-2.4.1\vendor\autoload.php';
+$client = new \Google_Client();
+$client->setApplicationName('GBUJobs');
+$client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+$client->setAccessType('online');
+$client->setAuthConfig(__DIR__ . '/api.json');
+$service = new Google_Service_Sheets($client);
+$spreadsheetId = "1W89pRZ1FTBrc04st-h7nhk8iuPeIc85dBMr1byAV64U";
+$range = 'responses';
+$response = $service->spreadsheets_values->get($spreadsheetId, $range);
+$values = $response->getValues();
+?>
